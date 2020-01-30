e987047b54f3ab5b03a49fdac89df11c09d84ee7
gulshanyadav01 <gulshany01@gmail.com>
2020-01-30 13:39:28 +0530

    second

---
+++ stu.java
@@ -0,0 +1,20 @@
+public class stu{
+	// public String name;
+	// public int age;
+	private String name;
+	private int age;
+	public void setname(String name){
+		this.name=name;
+	}
+	public String getname(){
+		return name;
+	}
+	public void setage(int age){
+		this.age=age;
+
+	}
+	public int getage(){
+		return (age);
+	}
+
+}
